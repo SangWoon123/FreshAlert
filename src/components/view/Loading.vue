f16d58b605b00b99dcc6235cd72fe9dfc342743d
SangWoon123 <alrax3574@naver.com>
2024-09-11 01:56:53 +0900

    feat: 로딩 컴포넌트

--- src/components/view/Loading.vue
+++ src/components/view/Loading.vue
@@ -1,13 +1,38 @@
+<script setup></script>
+
 <template>
-  <div>
-    
+  <div class="loading">
+    <div class="loading__logo">
+      <div>
+        <span>작지만 강한 친구</span>
+        <h2>스몰벗 유제품 기한</h2>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup>
+<style scope lang="scss">
+.loading {
+  height: 100%;
+  background: #3e8f88;
+  background-size: 30px 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-</script>
-
-<style lang="scss" scoped>
-
+  &__logo {
+    width: 100%;
+    height: 30%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    h2,
+    div {
+      color: white;
+    }
+    span {
+      color: #616870;
+    }
+  }
+}
 </style>
