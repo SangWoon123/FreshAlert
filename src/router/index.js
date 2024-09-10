9568883aa03624ba123e9f1fe325c25a037af8ae
SangWoon123 <alrax3574@naver.com>
2024-09-11 03:43:19 +0900

    feat: 라우터 컴포넌트

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '@/components/view/LoginPage.vue'
+import ProductList from '@/components/view/ProductList.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{ path: '/', component: LoginPage }]
+  routes: [
+    { path: '/', component: LoginPage },
+    { path: '/product', component: ProductList }
+  ]
 })
 
 export default router
