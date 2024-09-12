a5c8ee4ee069c9cb9aea3666c0af730864dc9989
SangWoon123 <alrax3574@naver.com>
2024-09-12 15:43:17 +0900

    fix: main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createVuetify } from 'vuetify/lib/framework.mjs'
 import * as components from 'vuetify/components'
+import { VDateInput } from 'vuetify/labs/VDateInput'
 import App from './App.vue'
 import router from './router'
 import '@/assets/font.css'
@@ -11,14 +12,18 @@ import '@/assets/font.css'
 const app = createApp(App)
 
 const vuetify = createVuetify({
-  components,
+  components: {
+    ...components,
+    VDateInput
+  },
   theme: {
     defaultTheme: 'custom',
     themes: {
       custom: {
         dark: false,
         colors: {
-          green: '#3e8f88'
+          green: '#3e8f88',
+          red: '#C2546E'
         }
       }
     }
