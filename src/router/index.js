b270a97924fc5cd32cd117efd295547a789c5010
SangWoon123 <alrax3574@naver.com>
2024-09-12 15:42:20 +0900

    fix: 라우터

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '@/components/view/LoginPage.vue'
 import ProductList from '@/components/view/ProductList.vue'
+import DiaryProductAdd from '@/components/modal/DiaryProductAdd.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: LoginPage },
-    { path: '/product', component: ProductList }
+    { path: '/product', component: ProductList },
+    { path: '/test', component: DiaryProductAdd },
   ]
 })
 
