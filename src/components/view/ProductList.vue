0d616dded4d61c2e5976744c4846c7b1b590d3ca
SangWoon123 <alrax3574@naver.com>
2024-09-12 15:45:00 +0900

    fix: 제품리스트 컴포넌트

--- src/components/view/ProductList.vue
+++ src/components/view/ProductList.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="product">
-    <h2>재고파악</h2>
+    <div class="header">
+      <h2>재고파악</h2>
+      <v-btn color="red" @click="deleteProduct">삭제</v-btn>
+    </div>
     <div class="search">
       <v-text-field density="compact" variant="outlined"> </v-text-field>
       <v-btn color="green">검색</v-btn>
@@ -13,33 +16,98 @@
     </div>
     <v-divider thickness="3px"></v-divider>
     <div class="list">
-      <div v-for="(item, index) in items" :key="index" class="list-item">
+      <div v-for="(item, index) in productStore.productList" :key="index" class="list-item">
         <span :style="style(index)">{{ item.name }}</span>
-        <span :style="style(index)">{{ item.expiry }}</span>
+        <span :style="style(index)">{{ item.expiration }}</span>
         <span :style="style(index)">{{ item.quantity }}</span>
         <input type="checkbox" v-model="item.checked" />
       </div>
     </div>
 
     <div class="footer">
-      <v-btn color="green" variant="outlined">유통기한 등록</v-btn>
-      <v-btn color="green">제품명 등록</v-btn>
+      <v-btn color="green" variant="outlined" @click="openDiaryModal">유통기한 등록</v-btn>
+      <v-btn color="green" @click="openProductModal">제품명 등록</v-btn>
+      <v-btn color="red" @click="openDeleteModal">제품명 삭제</v-btn>
+    </div>
+
+    <!-- 팝업배경흐리게 -->
+    <div v-if="productModal.isOpen.value" class="backdrop">
+      <div class="add-dialog" @click.stop>
+        <ProductNameAdd @close="closeProductModal" />
+      </div>
+    </div>
+
+    <div v-if="diaryModal.isOpen.value" class="backdrop">
+      <div class="add-dialog" @click.stop>
+        <DiaryProductAdd @close="closeDiaryModal" />
+      </div>
+    </div>
+
+    <div v-if="deleteModal.isOpen.value" class="backdrop">
+      <div class="add-dialog">
+        <DiaryProductDelete @close="closeDeleteModal" />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import ProductNameAdd from '../modal/ProductNameAdd.vue'
+import DiaryProductAdd from '../modal/DiaryProductAdd.vue'
+import DiaryProductDelete from '../modal/DiaryProductDelete.vue'
+import { useModal } from '@/util/useModal'
+import { authInstance } from '@/api/authApi'
+import { useProductList } from '@/stores/product'
+import { onMounted } from 'vue'
+import { watch } from 'vue'
 
-const items = ref([
-  { name: '[서울우유] 동서플레인요거트', expiry: '2024.10.25', quantity: 5, checked: false },
-  { name: '[매일유업] 소프트요거트', expiry: '2024.09.30', quantity: 3, checked: false }
-])
+//모달 상태
+const productModal = useModal()
+const diaryModal = useModal()
+const deleteModal = useModal()
+// 상태관리
+const productStore = useProductList()
+
+// 모달제어함수
+const openProductModal = () => productModal.open()
+const closeProductModal = () => productModal.close()
+
+const openDiaryModal = () => diaryModal.open()
+const closeDiaryModal = () => diaryModal.close()
+
+const openDeleteModal = () => deleteModal.open()
+const closeDeleteModal = () => deleteModal.close()
+
+const products = ref([])
+onMounted(async () => {
+  const response = await authInstance('/products').get('')
+  productStore.productList = response.data
+})
+
 const style = (index) => {
   return {
-    textDecoration: items.value[index].checked ? 'line-through' : 'none'
+    textDecoration: productStore.productList[index].checked ? 'line-through' : 'none'
   }
 }
+
+async function deleteProduct() {
+  // const deleteList = productStore.productList.filter((item) => item.checked === true)
+  // const response = await authInstance('/product').delete('', { deleteList })
+  console.log('제품 삭제')
+}
+
+watch(
+  products,
+  (newItems) => {
+    newItems.forEach((item) => {
+      if (item.checked && !item.delete_date) {
+        item.delete_date = new Date().toISOString()
+      }
+    })
+  },
+  { deep: true }
+)
 </script>
 
 <style lang="scss" scoped>
@@ -48,6 +116,10 @@ const style = (index) => {
   height: 100%;
   background-color: #ffffff;
   position: relative;
+  .header {
+    display: flex;
+    justify-content: space-between;
+  }
   h2 {
     margin-bottom: 20px;
   }
@@ -70,7 +142,7 @@ const style = (index) => {
     :nth-child(2) {
       display: flex;
       justify-content: center;
-      flex: 5;
+      flex: 4;
     }
     :nth-child(3) {
       display: flex;
@@ -106,7 +178,7 @@ const style = (index) => {
       font-size: 12px;
       display: flex;
       gap: 10px;
-      padding: 5px;
+      padding: 10px;
       justify-content: space-between;
       align-items: center;
       border-bottom: 1px solid #ddd;
@@ -121,10 +193,29 @@ const style = (index) => {
     position: fixed;
     bottom: 0;
     left: 0;
-
+    z-index: 2;
     border-top: 1px solid #ddd;
+    background-color: #ffffff;
     display: flex;
     justify-content: space-around;
+    align-items: center;
+  }
+
+  .backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.5); /* 뒷배경 흐리게 */
+    z-index: 1;
+  }
+  .add-dialog {
+    padding: 10px;
+    z-index: 2;
+    height: 100%;
+    display: flex;
+    justify-content: center;
     align-items: center;
   }
 }
