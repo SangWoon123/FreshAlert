7e57395ec58fb3e730aa0f25bf5caf0c76ba5fd1
SangWoon123 <alrax3574@naver.com>
2024-09-17 16:42:56 +0900

    fix: vue setting

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,19 +3,27 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createVuetify } from 'vuetify/lib/framework.mjs'
+import '@mdi/font/css/materialdesignicons.css'
 import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 import { VDateInput } from 'vuetify/labs/VDateInput'
 import App from './App.vue'
 import router from './router'
 import '@/assets/font.css'
-
+import { ko } from 'vuetify/locale'
 const app = createApp(App)
 
 const vuetify = createVuetify({
+  locale: {
+    locale: 'ko',
+    messages: { ko }
+  },
   components: {
     ...components,
+    directives,
     VDateInput
   },
+  ssr: true,
   theme: {
     defaultTheme: 'custom',
     themes: {
