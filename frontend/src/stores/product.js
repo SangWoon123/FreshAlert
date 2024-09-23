1a08120c0d230b89961283845d98ed2be1c47dc1
SangWoon123 <alrax3574@naver.com>
2024-09-24 00:37:03 +0900

    fix: product store

--- frontend/src/stores/product.js
+++ frontend/src/stores/product.js
@@ -2,11 +2,17 @@ import { defineStore } from 'pinia'
 
 export const useProductList = defineStore('product-list', {
   state: () => ({
-    productList: []
+    productList: [],
+    productNameList: [],
+    // 날짜 모음
+    dateList: []
   }),
   actions: {
     updateList(product) {
       this.productList.push(product)
+    },
+    updateDateList() {
+      this.dateList = [...new Set(this.productList.filter((item) => item.expiration))]
     }
   }
 })
