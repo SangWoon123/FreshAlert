6dc0bf49792b2214c5e2bd37ff30365c14de2a0b
SangWoon123 <alrax3574@naver.com>
2024-10-07 22:33:40 +0900

    fix: 카테고리 서브 데이터 리스트

--- frontend/src/stores/product.js
+++ frontend/src/stores/product.js
@@ -2,7 +2,9 @@ import { defineStore } from 'pinia'
 
 export const useProductList = defineStore('product-list', {
   state: () => ({
+    // 기본유제품
     productList: [],
+    categoryProductList: [],
     productNameList: [],
     // 날짜 모음
     dateList: []
