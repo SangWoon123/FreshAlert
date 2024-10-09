16fca32818e88628db7ca60000eecdc51171fa6f
SangWoon123 <alrax3574@naver.com>
2024-10-09 21:25:21 +0900

    fix: 사이드메뉴

--- frontend/src/components/ToggleMenu.vue
+++ frontend/src/components/ToggleMenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card style="z-index: 1;">
+  <v-card style="z-index: 1">
     <v-layout>
       <v-app-bar color="green" prominent>
         <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
@@ -16,12 +16,16 @@
       </v-app-bar>
 
       <v-navigation-drawer
-      width="150"
+        width="150"
         v-model="drawer"
         :location="$vuetify.display.mobile ? 'right' : undefined"
         temporary
       >
-        <v-list  :items="items"></v-list>
+        <v-list>
+          <v-list-item v-for="item in items" :key="item.value" @click="selectCategory(item.value)">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
       </v-navigation-drawer>
     </v-layout>
   </v-card>
@@ -30,15 +34,21 @@
 <script setup>
 import { ref, watch } from 'vue'
 
+const emit = defineEmits(['categoryChanged'])
+const selectCategory = (category) => {
+  emit('categoryChanged', category)
+  drawer.value = false
+}
+
 const items = [
   {
     title: '홈',
     value: 'home'
   },
   {
-    title: '카테고리',
+    title: '카테고리 추가',
     value: 'category'
-  },
+  }
 ]
 
 const drawer = ref(false)
