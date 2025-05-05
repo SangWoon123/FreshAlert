3d8b8902b71d68455161ba342f0b697f43558a5f
SangWoon123 <alrax3574@naver.com>
2025-05-05 18:35:54 +0900

    refactor: 새로운 메인 UI

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,13 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, sans-serif;
+}
 </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '@/components/view/LoginPage.vue'
-import ProductList from '@/components/view/ProductList.vue'
+import Ato from '@/components/content/Ato.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: LoginPage },
-    { path: '/product', component: ProductList }
+    { path: '/product', component: () => import('@/components/content/NewProduct.vue') },
+    { path: '/ato', component: Ato }
   ]
 })
 
